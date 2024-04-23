a7479d14cd8df51046ad1ebefca3a731f9c14517
Jyublee <sajuadish@gmail.com>
2024-04-23 17:39:48 +0530

    Overwrite

--- app.py
+++ app.py
@@ -10,26 +10,30 @@ genai.configure(api_key=my_api_key_gemini)
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("check.html")
+    return render_template('home.html')
 
-@app.route("/recommendation",methods=["POST"]) 
-def  recommendation():
-    user_url = request.form.get("url")
-    user_need = request.form.get("need")
-    prediction=pred(user_url)
-    try:
-        query = "Give me alternate websites to use for "+user_need+" in the format of www.domain_name.com"
-        response = model.generate_content(query)
-        links=extract_links(response.text)
-        links.insert(0,prediction)
-        if response.text:
-            return render_template("response.html", output=links)
-        else:
-            return render_template("response.html", output="Sorry, but we didn't get an answer to that!")
-    except Exception as e:
-        return render_template("response.html", output="Sorry, but we didn't get an answer to that!")
-    
-if __name__ == "__main__":
-  app.run(debug=True)
+@app.route('/link_check')
+def link_check():
+    return render_template('link_check.html')
+
+@app.route('/link_results', methods=['POST'])
+def link_results():
+    site_url = request.form['site_url']
+    site_functionality = request.form['site_functionality']
+    # Call your function to generate alternate links
+    links = generate_links(site_url, site_functionality)
+    label = "Alternate links for {} ({})".format(site_url, site_functionality)
+    return render_template('link_results.html', links=links, label=label)
+
+@app.route('/quiz')
+def quiz():
+    question = "What is the capital of France?"
+    options = ["Paris", "London", "Berlin", "Madrid"]
+    return render_template('quiz.html', question=question, options=options)
+
+@app.route('/submit_answer', methods=['POST'])
+def submit_answer():
+    # Handle the submitted answer
+    pass

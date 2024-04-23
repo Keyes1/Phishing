bb35dcf8386f2590aa8396be7e7a59be170017f7
Jyublee <sajuadish@gmail.com>
2024-04-23 17:53:02 +0530

    Updated with Link Check

--- app.py
+++ app.py
@@ -7,6 +7,24 @@ model = genai.GenerativeModel('gemini-pro')
 my_api_key_gemini = 'AIzaSyAmulpvGbEpbEISbq_VwhpqZYUfs2pHq8k'
 
 genai.configure(api_key=my_api_key_gemini)
+'''
+def  recommendation():
+    user_url = request.form.get("url")
+    user_need = request.form.get("need")
+    prediction=pred(user_url)
+    try:
+        query = "Give me alternate websites to use for "+user_need+" in the format of www.domain_name.com"
+        response = model.generate_content(query)
+        links=extract_links(response.text)
+        links.insert(0,prediction)
+        if response.text:
+            return render_template("response.html", output=links)
+        else:
+            return render_template("response.html", output="Sorry, but we didn't get an answer to that!")
+    except Exception as e:
+        return render_template("response.html", output="Sorry, but we didn't get an answer to that!")
+'''
+
 
 app = Flask(__name__)
 
@@ -23,8 +41,14 @@ def link_results():
     site_url = request.form['site_url']
     site_functionality = request.form['site_functionality']
     # Call your function to generate alternate links
-    #links = generate_links(site_url, site_functionality)
-    links=["www.fb.com"]
+    prediction=pred(site_url)
+    try:
+        query = "Give me alternate websites to use for "+site_functionality+" in the format of www.domain_name.com"
+        response = model.generate_content(query)
+        links=extract_links(response.text)
+        links.insert(0,prediction)
+    except Exception as e:
+        links=["Sorry Sar Kitiyilla"]
     label = "Alternate links for {} ({})".format(site_url, site_functionality)
     return render_template('link_results.html', links=links, label=label)
 
